aedfb1eaafa75896bfb2a1083721ed96d555b58c
Benjamin Andrew Metha <methab@student.unimelb.edu.au>
2018-11-20 16:28:35 +1100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+We don't know what we're doing and we don't know why we're doing it.
---
+++ test_fitsreader.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+test_fitsreader()
+	filename = 'Datafiles/spectra/spec-4055-55359-0001.fits'
